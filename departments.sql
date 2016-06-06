81d78c538072390a6b91fe68fd600e1881fd04a9
Ino Kouki <k_ino@ic-com.co.jp>
2016-06-06 11:25:16 +0900

    完成版(filter未実装)

--- departments.sql
+++
@@ -1,7 +0,0 @@
-CREATE  TABLE `kadai4`.`departments` (
- 
- `id` INT NOT NULL AUTO_INCREMENT ,
- 
- `name` VARCHAR(20) NOT NULL ,
-  
-PRIMARY KEY (`id`) );
--- schema.sql
+++
@@ -1,1 +0,0 @@
-CREATE SCHEMA `kadai4` ;
